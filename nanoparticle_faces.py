2eef72e8c3004f1e04ca2c5ef47c561a237d3485
geoffreyweal <geoffrey.weal@gmail.com>
2022-01-20 21:29:15 -0800

    Update the Computational Silver Nanoparticle Exercise.

--- nanoparticle_faces.py
+++ nanoparticle_faces.py
@@ -38,7 +38,7 @@ def faces_of_nanoparticles():
     for n in atoms_111a:
         colours[n] = (188/255.0, 45/255.0, 255/255.0) #jmol_colors[atomic_numbers['Hf']] # 'Fe'
 
-    atoms_111b = [200, 189, 191, 147, 196, 195, 130, 146, 131, 169, 167, 115]
+    atoms_111b = [189, 147, 131, 115, 169, 196] #[200, 189, 191, 147, 196, 195, 130, 146, 131, 169, 167, 115]
     for n in atoms_111b:
         colours[n] = jmol_colors[atomic_numbers['Th']]  # (17/255.0, 138/255.0, 178/255.0) #jmol_colors[atomic_numbers['Ca']] # 'Mg' 
 
