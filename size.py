d2d87269dc9243e96d1ea3529fc3228f4eb2f724
geoffreyweal <geoffrey.weal@gmail.com>
2022-01-22 04:19:37 -0800

    Update the Computational Silver Nanoparticle Exercise.

--- size.py
+++ size.py
@@ -10,23 +10,26 @@ surfaces = [(1,0,0),(1,1,1)]
 surf_e_100 = 0.9
 surf_e_111 = 1.0
 
-def make_nanoparticle():
+def make_nanoparticle(size=None):
 	#### Change the size of the cluster below and see the changes ####
-	while True : 
-		size = input("What size nanoparticle do you want to make?: ")
-		if size.isdigit() :
-			size = int(size)
-			if 13 <= size :
-				if 12 <= size <= 11421 :  	
-					print(size)
-					break
-				else :
-					print("The size has to be between 13 and 11421. Try again.")
+	if size==None:
+		while True : 
+			size = input("What size nanoparticle do you want to make?: ")
+			if size.isdigit() :
+				size = int(size)
+				if 13 <= size :
+					if 12 <= size <= 11421 :  	
+						print(size)
+						break
+					else :
+						print("The size has to be between 13 and 11421. Try again.")
+				else : 
+					print("This is not enough atoms to make a nanoparticle. Try again.")
 			else : 
-				print("This is not enough atoms to make a nanoparticle. Try again.")
-		else : 
-			print("You have not entered a valid number. Try again.")
-
+				print("You have not entered a valid number. Try again.")
+	elif not isinstance(size,int):
+		print('Error: You input for size needs to be a int (integer). Change this and try again')
+		
 	esurf = [surf_e_100,surf_e_111]
 	atoms = wulff_construction('Ag',surfaces,esurf,size,'fcc',rounding='above')
 	atoms.cell=[0,0,0]
